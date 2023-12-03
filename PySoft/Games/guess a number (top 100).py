eacf588c3fd112d7257c8de4923fea9b6d64c940
GamerSoft24 <haoyun.benjamin.chi@gmail.com>
2023-12-03 09:30:42 +0100

    Uploaded games

---
+++ PySoft/Games/guess a number (top 100).py
@@ -0,0 +1,17 @@
+import random, time
+print('Guess a number from 1-100!')
+num = random.randint(1,100)
+guess = ''
+i = 0
+while guess != num:
+  i += 1
+  guess = int(input('Please input a number between 1-100: '))
+  if guess == num:
+    print('Congrats! You guessed correct!')
+  elif guess < num:
+    print('Your number was too small...')
+  else:
+    print('Your number was too big...')
+print('You guessed in total%d' %i + 'times', end = '')
+print("Quick! Go compare your score with your friends'!")
+time.sleep(3)
---
+++ PySoft/Games/rainbow void.py
@@ -0,0 +1,10 @@
+import turtle
+q = turtle.Pen()
+turtle.bgcolor("black")
+sides = 7
+colors = ["red","orange","yellow","green","cyan","blue","purple"]
+for x in range(360):
+  q.pencolor(colors[x%sides])
+  q.forward(x*3/sides+x)
+  q.left(360/sides+1)
+  q.width(x*sides/200)
