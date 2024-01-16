fc071bd036aa2a7bf00dc1b282dea498b9b89685
GS-ST <147323945+GS-ST@users.noreply.github.com>
2024-01-16 12:20:11 +0000

    Update software360.bat

--- BatchSoft/software360.bat
+++ BatchSoft/software360.bat
@@ -1,5 +1,12 @@
 @echo off
+goto star
 :START
 echo Software360 All-in-1 
 echo [1] UAC Bypass
 echo [2] UAC Bypass (Encrypted)
+echo [3] PLACEHOLDER
+echo [4] PLACEHOLDER
+choice /c:1234 /m Choose an option
+:UACBYPASS1
+rem USE SET /P TO GET PATHLINE INPUT AND SET %1 TO THE SET /P INPUT LINE
+cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%1"
