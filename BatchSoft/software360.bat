2565a06a4ec7901aae14d3041cb02ec633510a6c
GS-ST <147323945+GS-ST@users.noreply.github.com>
2024-01-17 13:44:07 +0000

    Update software360.bat gamersoft

--- BatchSoft/software360.bat
+++ BatchSoft/software360.bat
@@ -2,13 +2,13 @@
 goto start
 :START
 echo Software360 All-in-1 
-echo Made by GamerSoftware Corp. and Okmeque1 Computers. (c) All rights reserved.
-echo V0.33 Alpha stage. Beta stage at V0.5. If action returns to main menu, that means the option is not implemented.
-echo [1] UAC Bypass
-echo [2] UAC Bypass (Encrypted)
-echo [3] Make Elevated task
-echo [4] Start PROGRAM w/flags (e.g : using --user-data-dir and --disable-certificate-errors when starting BrStd1 Browser)
-echo [5] Quit
+echo (c) GamerSoftware Corporation™ and Okmeque1 Corporation™. All rights reserved.
+echo Program version: 0.88-alpha. If action returns to main menu, that means the option is not implemented.
+echo [1] UAC Bypass.
+echo [2] UAC Bypass (Encrypted).
+echo [3] Make Elevated Task.
+echo [4] Start PROGRAM w/flags (e.g : using --user-data-dir and --disable-certificate-errors when starting BrStd1 Browser).
+echo [5] Quit program.
 choice /c:12345 /m "Choose an option : "
 IF ERRORLEVEL 5 GOTO END
 IF ERRORLEVEL 4 GOTO BRSTD1
@@ -20,13 +20,13 @@ set /p input="Enter FILE PATH : "
 cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%input%"
 goto START
 :UACBYPASSENCRYPT
-REM Gamersoft! You figure out how it works and make the %1 variable to the command on line 17.
+REM GamerSoft24! You figure out how it works and make the %1 variable to the command on line 17.
 goto START
 :SETADMIN
 REM See, the CMDLine works using the bypass UAC but the problem is that it opens std cmd prompt and we want admin cmd prompt
 goto START
 :BRSTD1
-set /p BR="Enter program with or with no parameters. : "
+set /p BR="Enter program with or with no parameters: "
 start "%BR%"
 goto START
 :END
