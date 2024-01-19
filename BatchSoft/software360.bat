5a46d84b85248bfcc70b985d4599cacd01ce05a1
GS-ST <147323945+GS-ST@users.noreply.github.com>
2024-01-19 10:40:46 +0000

    upd

--- BatchSoft/software360.bat
+++ BatchSoft/software360.bat
@@ -6,19 +6,17 @@ echo Software360 All-in-1
 echo (c) GamerSoftware Corporation™ and Okmeque1 Corporation™. All rights reserved.
 echo Program version: 0.88-beta. If action returns to main menu, that means the option is not implemented.
 echo [1] UAC Bypass.
-echo [2] UAC Bypass-Encrypted.
-echo [3] Make Elevated Task.
-echo [4] Start PROGRAM w/flags (using --user-data-dir and --disable-certificate-errors when starting BrStd1 Browser).
-echo [5] Quit program.
+echo [2] Make Elevated Task.
+echo [3] Start PROGRAM w/flags (using --user-data-dir and --disable-certificate-errors when starting BrStd1 Browser).
+echo [4] Quit program.
 choice /c:12345 /m "Choose an option: "
-IF ERRORLEVEL 5 GOTO END
-IF ERRORLEVEL 4 GOTO BRSTD1
-IF ERRORLEVEL 3 GOTO SETADMIN
-IF ERRORLEVEL 2 GOTO UACBYPASSENCRYPT
+IF ERRORLEVEL 4 GOTO END
+IF ERRORLEVEL 3 GOTO BRSTD1
+IF ERRORLEVEL 2 GOTO SETADMIN
 IF ERRORLEVEL 1 GOTO UACBYPASS
 :UACBYPASS
 cls
-set /p input="Enter FILE PATH : "
+set /p input="Enter file path: "
 cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%input%"
 echo.
 goto START
