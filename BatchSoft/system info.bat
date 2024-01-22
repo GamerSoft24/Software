7e4f68db15334f1da393d80bee8fe24b021b8e87
GS-ST <147323945+GS-ST@users.noreply.github.com>
2024-01-22 14:48:12 +0000

    Add files via upload

---
+++ BatchSoft/system info.bat
@@ -0,0 +1,28 @@
+@echo off
+echo System Informations:
+echo.
+
+echo Running whoami...
+whoami
+
+echo.
+echo Running systeminfo...
+systeminfo
+
+echo.
+echo Running ipconfig...
+ipconfig
+
+echo.
+echo Listing directories of C:...
+dir
+
+echo.
+echo Displaying user information...
+net user
+
+echo.
+echo Listing currently running tasks...
+tasklist
+
+pause
