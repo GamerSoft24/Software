49dfbbb6c23262af6be65689932ec068c49c69eb
GS-ST <147323945+GS-ST@users.noreply.github.com>
2024-01-23 12:12:38 +0000

    Update asfd.bat

--- BatchSoft/Windows/asfd.bat
+++ BatchSoft/Windows/asfd.bat
@@ -1,3 +1,42 @@
 @echo off
 rem Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...
 echo.
+echo Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...Fixing batchfile language not appearing problem...
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problemecho Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problemecho Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
+echo Fixing batchfile language not appearing problem
