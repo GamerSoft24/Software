1ce11abb14705e8377aea086d774658bb9e67b0f
GamerAlt24 <153452901+GamerAlt24@users.noreply.github.com>
2024-01-31 13:41:47 +0000

    Update software360 aio.bat

--- BatchSoft/software360 aio.bat
+++ BatchSoft/software360 aio.bat
@@ -4,14 +4,14 @@ title Software360 AIO
 :START
 cls
 color a
-echo Software360 AIO:
-echo Made by GamerSoftware Corp. and Okmeque1 Computers. (c) All rights reserved.
+echo Software360 AIO/All-In-One:
 echo V1.26-beta. If action returns to main menu, that means the option is not implemented.
-echo [1] UAC Bypass
-echo [2] Goto CMD (cmd.exe).
-echo [3] Use COMPATABILITY flag for browser( disabling all errors and faking user agent. Uses SUPERMIUM 118 User agent and _BrStd1(taskbar) settings.)
-echo [4] Quit
-choice /c:12345 /m "Choose an option : "
+echo (c) GamerSoftware Corporation and Okmeque1 Corporation. All rights reserved.
+echo [1] UAC Bypass.
+echo [2] Open Windows Command Prompt (cmd.exe).
+echo [3] BRSTD1 (Browser Standard 1).
+echo [4] Quit program.
+choice /c:1234 /m "Choose an option : "
 IF ERRORLEVEL 4 GOTO END
 IF ERRORLEVEL 3 GOTO 360UDATA1
 IF ERRORLEVEL 2 GOTO STARTCMD
