9acb886ca96a942f7337b57ab14e7c6b903be8aa
GamerSoft24 <haoyun.benjamin.chi@gmail.com>
2024-02-07 13:25:21 +0000

    Update windows360 aio.bat

--- BatchSoft/Windows/windows360 aio.bat
+++ BatchSoft/Windows/windows360 aio.bat
@@ -11,7 +11,7 @@ echo If action returns to main menu, that means the option is not implemented.
 echo [1] System Infomations.
 echo [2] Website Pinger.
 echo [3] Quit program.
-choice /c:1234 /m "Choose an option: "
+choice /c:123 /m "Choose an option: "
 IF ERRORLEVEL 3 GOTO QUIT
 IF ERRORLEVEL 2 GOTO WEBPING
 IF ERRORLEVEL 1 GOTO SYSTEMINFOS
