47d7a7e9cd5a7abdcd134ebb2a0b82d04048b214
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2024-02-09 18:55:27 +0000

    Create 360Loader.bat

---
+++ BrowserSoft/Files/360AFChrome/bin-2036r1af/360Loader.bat
@@ -0,0 +1,2 @@
+start "" "%~dp0\3602036r1af_U1May23.exe" --user-data-dir=%~dp0\3602036r1afData1 --disable-infobars --no-sandbox --ignore-certificate-errors --disable-logging --no-default-browser-check --disable-component-update --disable-background-networking --allow-outdated-plugins --cipher-suite-blacklist=0xcc14,0xe013  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+exit /b
