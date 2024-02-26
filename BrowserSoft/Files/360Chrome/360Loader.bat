bfed3da77c475f0411307cc67c968eaea8a61436
GamerSoft24 <haoyun.benjamin.chi@gmail.com>
2024-02-26 12:08:37 +0000

    Delete "Files" in BrowserSoft due to "chrome.dll" files.

--- BrowserSoft/Files/360Chrome/360Loader.bat
+++
@@ -1,3 +0,0 @@
-:360Chrome Loader
-start "" "%~dp0\360chrome.exe" --user-data-dir=%~dp0\360UData1 --disable-infobars  --no-sandbox  --ignore-certificate-errors --disable-logging --no-default-browser-check --disable-component-update --disable-background-networking --allow-outdated-plugins --cipher-suite-blacklist=0xcc14,0xe013 --ignore-certificate-errors --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
-exit /b
