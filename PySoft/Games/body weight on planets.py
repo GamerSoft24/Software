f50dc73b0b487a6dd93408d087757395153551fd
GamerSoft24 <haoyun.benjamin.chi@gmail.com>
2024-03-17 11:56:46 +0000

    Create body weight on planets.py

---
+++ PySoft/Games/body weight on planets.py
@@ -0,0 +1,28 @@
+weight = int(input('Enter your weight in kg: '))
+moon = 16.5
+mercury = 40
+venus = 96
+mars = 38
+jupiter = 264
+saturn = 115
+uranus = 117
+neptune = 118
+pluto_not_planet = 42
+weight_moon = weight * moon / 100
+weight_mercury = weight * mercury / 100
+weight_venus = weight * venus / 100
+weight_mars = weight * mars / 100
+weight_jupiter = weight * jupiter / 100
+weight_saturn = weight * saturn / 100
+weight_uranus = weight * uranus / 100
+weight_neptune = weight * neptune / 100
+weight_pluto_not_planet = weight * pluto_not_planet / 100
+print('Your weight on the Moon is',weight_moon,'kg.')
+print('Your weight on Mercury is',weight_mercury,'kg.')
+print('Your weight on Venus is',weight_venus,'kg.')
+print('Your weight on Mars is',weight_mars,'kg.')
+print('Your weight on Jupiter is',weight_jupiter,'kg.')
+print('Your weight on Saturn is',weight_saturn,'kg.')
+print('Your weight on Uranus is',weight_uranus,'kg.')
+print('Your weight on Neptune is',weight_neptune,'kg.')
+print('Your weight on Pluto (not a planet) is',weight_pluto_not_planet,'kg.')
